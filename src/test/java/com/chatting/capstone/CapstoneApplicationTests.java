4904da1561741bdcf408556879e565e308333e3a
ahyeonkong <kimah0403@gmail.com>
2025-04-06 18:30:02 +0900

    fix: contextLoads() 에러 해결

--- src/test/java/com/chatting/capstone/CapstoneApplicationTests.java
+++ src/test/java/com/chatting/capstone/CapstoneApplicationTests.java
@@ -2,12 +2,14 @@ package com.chatting.capstone;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CapstoneApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
