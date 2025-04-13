124b9037b2bd7ea079ed66ae537f143e3e79b88c
ahyeonkong <kimah0403@gmail.com>
2025-04-13 16:29:45 +0900

    fix: RDS 환경에서 room_id가 NULL로 삽입되는 문제 해결

--- src/main/java/com/chatting/capstone/global/config/RoomInitializer.java
+++ src/main/java/com/chatting/capstone/global/config/RoomInitializer.java
@@ -2,27 +2,27 @@ package com.chatting.capstone.global.config;
 
 import com.chatting.capstone.domain.room.entity.Room;
 import com.chatting.capstone.domain.room.repository.RoomRepository;
-import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
 import java.util.ArrayList;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class RoomInitializer {
-
+public class RoomInitializer implements CommandLineRunner {
     private final RoomRepository roomRepository;
 
-    @PostConstruct
-    public void init() {
+    @Override
+    @Transactional
+    public void run(String... args) {
         createRoomIfNotExists("Room 1");
         createRoomIfNotExists("Room 2");
         createRoomIfNotExists("Room 3");
     }
 
     private void createRoomIfNotExists(String roomName) {
-        boolean exists = roomRepository.existsByRoomName(roomName);
-        if (!exists) {
+        if (!roomRepository.existsByRoomName(roomName)) {
             roomRepository.save(
                     Room.builder()
                             .roomName(roomName)
