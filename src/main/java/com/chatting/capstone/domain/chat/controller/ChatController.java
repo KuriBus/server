173fa979c5b2351eedac6cb223ce25d780adeba9
석해현 <gogus4600@kyonggi.ac.kr>
2025-04-18 23:22:13 +0900

    feat/ #7 채팅 db저장 및 룸별 채팅 구현

--- src/main/java/com/chatting/capstone/domain/chat/controller/ChatController.java
+++ src/main/java/com/chatting/capstone/domain/chat/controller/ChatController.java
@@ -7,7 +7,6 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
