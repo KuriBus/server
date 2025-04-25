035e0d2a3c0d14cdb75f1668d191a6edcd614ea5
ahyeonkong <kimah0403@gmail.com>
2025-04-25 19:25:01 +0900

    feat: Room 엔티티에 좌표 제한 및 포탈 관련 필드 추가

--- src/main/java/com/chatting/capstone/domain/room/repository/RoomRepository.java
+++ src/main/java/com/chatting/capstone/domain/room/repository/RoomRepository.java
@@ -1,8 +1,10 @@
 package com.chatting.capstone.domain.room.repository;
+import java.util.Optional;
 
 import com.chatting.capstone.domain.room.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
     boolean existsByRoomName(String roomName);
+    Optional<Room> findByRoomName(String roomName);
 }
