bbd6f56bbd507a1a5d3ef4f05499aa6fe6f8b33b
석해현 <gogus4600@kyonggi.ac.kr>
2025-06-08 20:01:15 +0900

    feat/#86-customization-all 모든 유저 에셋 불러오기

--- src/main/java/com/chatting/capstone/domain/customization/controller/CustomizationController.java
+++ src/main/java/com/chatting/capstone/domain/customization/controller/CustomizationController.java
@@ -1,8 +1,12 @@
 package com.chatting.capstone.domain.customization.controller;
 
 import com.chatting.capstone.domain.customization.dto.request.CustomizationRequest;
+import com.chatting.capstone.domain.customization.dto.response.CustomizationResponse;
+import com.chatting.capstone.domain.customization.entity.Customization;
 import com.chatting.capstone.domain.customization.service.CustomizationService;
 import com.chatting.capstone.global.response.ApiResponse;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.chatting.capstone.global.response.ResponseStatus;
@@ -23,5 +27,11 @@ public class CustomizationController {
         return ResponseEntity
                 .status(ResponseStatus.CUSTOMIZATION_UPDATE_SUCCESS.getStatus())
                 .body(ApiResponse.of(ResponseStatus.CUSTOMIZATION_UPDATE_SUCCESS));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<CustomizationResponse>> getAllCustomizations() {
+        List<CustomizationResponse> responses = customizationService.getAllCustomizations();
+        return ResponseEntity.ok(responses);
     }
 }
